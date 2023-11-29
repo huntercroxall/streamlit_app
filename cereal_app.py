3533ed84a20a8e11845f9cd1dd1e117a6709d03c
“First <h.j.croxall@gmail.com>
2023-11-29 16:53:46 -0700

    made a couple changes

--- cereal_app.py
+++ cereal_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("Fun App for Cereal")
 
 df = pd.read_csv("cereal.csv")
 st.dataframe(df)
+
+st.subheader("Histogram")
+
+st.bar_chart(np.histogram(df['calories'], bins=10))
