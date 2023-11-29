cbe48987c283b03a29cd6c67015b1a94692b194c
“First <h.j.croxall@gmail.com>
2023-11-29 16:08:25 -0700

    downloaded data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
 
 st.title("Hello World")
+
+penguins = sns.load_dataset('penguins')
+
+st.subheader('Raw Data')
+st.write(penguins)
