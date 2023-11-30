d05d33fdc7e3b0290abaf74328db2fd3bc35ad39
“First <h.j.croxall@gmail.com>
2023-11-29 22:45:26 -0700

    deleted some code

--- cereal_app.py
+++ cereal_app.py
@@ -1,13 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-st.subheader("Fun App for Cereal")
+
+st.title("Fun App for Cereal")
 
 df = pd.read_csv("cereal.csv")
-st.dataframe(df)
-
-st.subheader("Histogram")
-
-st.bar_chart(sns.countplot(penguins, x="species"))
