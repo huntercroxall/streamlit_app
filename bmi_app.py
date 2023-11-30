7a103720e32f144404ab5bd6dcc0372b4292dafb
“First <h.j.croxall@gmail.com>
2023-11-30 10:52:06 -0700

    Made changes to the BMI calculator

--- bmi_app.py
+++ bmi_app.py
@@ -9,29 +9,38 @@ st.bar_chart(chart_data)
 
 
 def calculate_bmi(height, weight):
-    height_meters = height / 100
+    height_meters = height/100
     bmi = weight / (height_meters ** 2)
     return bmi
 
 st.title("Height and Weight Visualization")
 
-height = st.slider("Select Height (inches)", 30, 100, 75)
+height = st.slider("Select Height (centimeters)", 100, 200, 150)
 
-weight = st.slider("Select Weight (pounds)", 20, 300, 120)
+weight = st.slider("Select Weight (kilograms)", 36, 120, 70)
 
 bmi = calculate_bmi(height, weight)
 
 st.subheader(f"**BMI:** {bmi:.2f}")
+
+if bmi < 18.5:
+    st.write("Underweight")
+elif bmi >= 18.5 and bmi <= 25:
+    st.write("Normal/Healthy Weight")
+elif bmi > 25 and bmi < 30:
+    st.write("Overweight")
+elif bmi >= 30:
+    st.write("Obese")
 
 #st.line_chart({"Height": [height], "Weight": [weight]})
 
 fig, ax = plt.subplots()
 ax.scatter(height, weight, color='blue', s = 100, label='Individual')
 
-ax.set_xlim(left=0, right=100)
-ax.set_ylim(bottom=0, top=300)
-ax.set_xlabel('Height in inches')
-ax.set_ylabel('Weight in pounds')
+ax.set_xlim(left=100, right=200)
+ax.set_ylim(bottom=20, top=150)
+ax.set_xlabel('Height in centimeters')
+ax.set_ylabel('Weight in kilograms')
 ax.set_title('Height vs. Weight')
 ax.legend()
 
