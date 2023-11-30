25607894d1475a628fd44a1fa95d5732b922ef75
“First <h.j.croxall@gmail.com>
2023-11-29 17:04:40 -0700

    made a couple more changes

--- cereal_app.py
+++ cereal_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("Fun App for Cereal")
+st.subheader("Fun App for Cereal")
 
 df = pd.read_csv("cereal.csv")
 st.dataframe(df)
 
 st.subheader("Histogram")
 
-st.bar_chart(np.histogram(df['calories'], bins=10))
+st.bar_chart(sns.countplot(penguins, x="species"))
